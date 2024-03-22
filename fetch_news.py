dcd3b981e9e5be89891442562ec4b59d9ab29513
sdlkhfksl <97817853+sdlkhfksl@users.noreply.github.com>
2024-03-22 13:23:31 +0800

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -1,6 +1,7 @@
 import feedparser
 import requests
 import re
+import time
 from datetime import datetime
 
 # 创建会话并设置用户代理
@@ -9,10 +10,11 @@ session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0
 
 # 定义重定向和格式化URL的函数
 def reformat_url(url):
-    result = re.sub('https://cryptopanic.com/news/(\\d+)/.*', 'https://cryptopanic.com/news/click/\\1/', url)
+    result = re.sub('https://cryptopanic.com/news/(\\\\d+)/.*', 'https://cryptopanic.com/news/click/\\\\1/', url)
     return result
 
 def get_real_url(re_url):
+    time.sleep(10)  # 添加的延时
     try:
         response = session.get(re_url, timeout=5)
         response.raise_for_status()
@@ -35,14 +37,14 @@ feed = feedparser.parse(feed_url)
 for entry in feed.entries:
     formatted_url = reformat_url(entry.link)
     final_url = get_real_url(formatted_url)
-    
+
     if final_url and final_url not in accumulated_links:
         accumulated_links.add(final_url)  # 添加新链接到集合中
 
 # 将新链接写入文件
 with open('accumulated_links.txt', 'w') as file:
     for link in accumulated_links:
-        file.write(link + '\n')
+        file.write(link + '\\n')
 
 # 打印所有链接，可选操作
 for link in accumulated_links:
